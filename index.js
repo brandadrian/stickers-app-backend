e4d2ff050dd86acc7098ec0429dbc021d1f3fac6
brandadrian <brandadrian@gmail.com>
2024-04-21 21:04:08 +0200

    initial

---
+++ index.js
@@ -0,0 +1,47 @@
+var Express = require('express');
+var MongoClient = require("mongodb").MongoClient;
+var cors = require('cors');
+require("dotenv").config();
+
+var app = Express();
+app.use(cors());
+app.use(Express.json());
+
+var databaseConnectionString = process.env.DB_CONNECTION_STRING
+var databaseName = process.env.DB_NAME
+
+var database;
+
+app.listen(5050, () => {
+    MongoClient.connect(databaseConnectionString,(error,client) =>{
+        database=client.db(databaseName);
+        console.log("Database connected");
+    });
+
+    console.log("Server started...");
+});
+
+app.get('/api/stickersapp/stickers', (request, response) => {
+    database.collection("stickersappcollection").find({}).toArray((error, result) =>{
+        response.send(result);
+    })
+})
+
+app.post('/api/stickersapp/stickers', (request, response) => {
+    database.collection("stickersappcollection").count({}, function(error, numOfDocs) {
+
+        database.collection("stickersappcollection").insertOne({
+            id: (numOfDocs+1).toString(),
+            name: request.body.name,
+            country: request.body.country
+        });
+        response.json("Added successfully");
+    })
+})
+
+app.delete('/api/stickersapp/stickers', (request, response)=> {
+    database.collection("stickersappcollection").deleteOne({
+        id: request.query.id
+    });
+    response.json("Deleted successfully");
+})
